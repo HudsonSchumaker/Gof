7226f7595511073a29631180d84a7fbb71524cb1
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2020-10-26 20:04:57 +0100

    Update VitaminBox.java

--- src/br/com/schumaker/nogof/creational/multion/example3/VitaminBox.java
+++ src/br/com/schumaker/nogof/creational/multion/example3/VitaminBox.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.schumaker.nogof.creational.multion.example3;
 
 import java.util.ArrayList;
