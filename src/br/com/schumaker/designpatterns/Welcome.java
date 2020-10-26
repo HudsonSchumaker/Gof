35d222b011d4779828f7a894e178bf90f2b35022
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2020-10-26 20:04:16 +0100

    Update Welcome.java

--- src/br/com/schumaker/designpatterns/Welcome.java
+++ src/br/com/schumaker/designpatterns/Welcome.java
@@ -6,7 +6,6 @@ package br.com.schumaker.designpatterns;
  * @since 17/09/2014
  */
 public class Welcome {
-
     public static void main(String[] args) {
         System.out.println("Welcome to design patterns!!!");
         System.out.println("Bem-vindo a design patterns!!!");
