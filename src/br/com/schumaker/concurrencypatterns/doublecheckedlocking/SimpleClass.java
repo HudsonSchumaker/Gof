063d5a83304c99c2f46008b7d092d2e1548b0f0e
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2021-08-02 13:58:41 +0200

    Update SimpleClass.java

--- src/br/com/schumaker/concurrencypatterns/doublecheckedlocking/SimpleClass.java
+++ src/br/com/schumaker/concurrencypatterns/doublecheckedlocking/SimpleClass.java
@@ -2,7 +2,6 @@ package br.com.schumaker.concurrencypatterns.doublecheckedlocking;
 
 /**
  * @author Hudson Schumaker
- * @since 18/09/2014
  */
 public class SimpleClass {
     public SimpleClass() {
