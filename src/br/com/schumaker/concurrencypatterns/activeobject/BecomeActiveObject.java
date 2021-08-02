5a6f95e979c9e3f74aebb6762e8fa8afdcd03686
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2021-08-02 14:00:35 +0200

    Update BecomeActiveObject.java

--- src/br/com/schumaker/concurrencypatterns/activeobject/BecomeActiveObject.java
+++ src/br/com/schumaker/concurrencypatterns/activeobject/BecomeActiveObject.java
@@ -7,6 +7,7 @@ import java.util.concurrent.LinkedBlockingQueue;
  * @author Hudson Schumaker
  */
 public class BecomeActiveObject {
+    
     private double val = 0.0;
     private boolean isRunning;
     private final BlockingQueue<Runnable> dispatchQueue;
